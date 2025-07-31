1e0844174fb77a378104dda2e26bb7ae21a32f31
lucas6180 <lucasricardo525@gmail.com>
2025-07-31 16:57:16 -0300

    Alteração no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/menuBar.js',
+                'resources/js/logout.js',
+                'resources/js/showUrl.js',
+                'resources/js/alert.js',
+                'resources/js/links.js',
+                'resources/js/loading-screen.js',
+                'resources/js/index.js',
+                'resources/js/pasteLink.js',
+            ],
             refresh: true,
         }),
     ],
