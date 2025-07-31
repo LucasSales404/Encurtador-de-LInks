a8171d6c864e4a6e5a702db108f542dd7f85b188
Lucas Sales <143664191+LucasSales404@users.noreply.github.com>
2025-07-31 14:38:33 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# Substitui a variável $PORT no arquivo de configuração do Nginx
+set -e
+echo "==> Iniciando script start.sh"
+echo "PORT = $PORT"
+
 envsubst '$PORT' < /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default
+cat /etc/nginx/sites-enabled/default
 
-# Inicia o PHP-FPM em background
 php-fpm -D
-
-# Inicia o Nginx em foreground (mantém o container rodando)
 nginx -g 'daemon off;'
