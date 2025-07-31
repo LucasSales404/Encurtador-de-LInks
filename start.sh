5c771f61bc2c809ed294cb048de45704e186e5b9
Lucas Sales <143664191+LucasSales404@users.noreply.github.com>
2025-07-31 14:43:40 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-set -e
-echo "==> Iniciando script start.sh"
-echo "PORT = $PORT"
-
-envsubst '$PORT' < /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default
-cat /etc/nginx/sites-enabled/default
-
 php-fpm -D
 nginx -g 'daemon off;'
