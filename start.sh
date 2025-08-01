7aaa7bee8b97821c006099e3752120cfe30c0bc3
Lucas Sales <143664191+LucasSales404@users.noreply.github.com>
2025-07-31 21:34:28 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ set -e
 # Rodar migrations automaticamente no start
 php artisan migrate --force || true
 
+# Iniciar o Nginx
+service nginx start
+
+# Manter o container vivo com o PHP-FPM em foreground
 exec php-fpm
